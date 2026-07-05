42107cc286f0362965ac0538eef52ad3145f8632
Jherson1017 <jhersonalaya10@gmail.com>
2026-07-05 15:07:49 -0500

    Método para insertar

--- Clases/ListaVertices.cs
+++ Clases/ListaVertices.cs
@@ -8,5 +8,17 @@ namespace Clases
 {
     public class ListaVertices
     {
+        public Vertice primero = null;
+
+        public void Insertar(Agencia a)
+        {
+            Vertice nuevo = new Vertice();
+
+            nuevo.dato = a;
+
+            nuevo.sig = primero;
+
+            primero = nuevo;
+        }
     }
 }
